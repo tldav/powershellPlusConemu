311f63f862536a7bcce562f736cd77a57b4b9a45
tldav <tldavis09@yahoo.com>
2022-12-22 14:50:31 -0600

    add function to remove default aliases for gcm and gp when installing misc commands module

--- write/writeProfileReqs.psm1
+++ write/writeProfileReqs.psm1
@@ -10,4 +10,29 @@ function overrideGciAlias {
   }
 }
 
-Export-ModuleMember overrideGciAlias
+function overrideGitAlias {
+  $profileDir = $profile.substring(0, $profile.indexOf("\Microsoft.PowerShell_profile.ps1"))
+  $module = "$profileDir\Modules\miscCommands\miscCommands.psm1"
+
+  $isGcmInstalled = Select-String -Path $module -Pattern "New-Alias -Name gcm -Value Invoke-GitCommitM"
+
+  $isGCMdefaultRemoved = Select-String -Path $profile -Pattern "Remove-Item alias:gcm -Force"
+
+  if ($isGcmInstalled -and -not $isGCMdefaultRemoved) {
+    Add-Content $profile "`nif ([bool] (Get-Command -ErrorAction Ignore Invoke-GitCommitM)) {"
+    Add-Content $profile "`tRemove-Item alias:gcm -Force"
+    Add-Content $profile "}`n"
+  }
+
+
+  $isGpiInstalled = Select-String -Path $module -Pattern "function Invoke-GitPush {"
+  $isGPIdefaultRemoved = Select-String -Path $profile -Pattern "Remove-Item alias:gp -Force"
+
+  if ($isGpiInstalled -and -not $isGPIdefaultRemoved) {
+    Add-Content $profile "`nif ([bool] (Get-Command -ErrorAction Ignore Invoke-GitPush)) {"
+    Add-Content $profile "`tRemove-Item alias:gp -Force"
+    Add-Content $profile "}`n"
+  }
+}
+
+Export-ModuleMember overrideGciAlias, overrideGitAlias
