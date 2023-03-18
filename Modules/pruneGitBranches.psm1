05d0894fafa90d186a7b805721b2d52fd1653382
Thomas Davis <tldavis09@yahoo.com>
2023-03-18 18:03:28 -0500

    refactored prune git branches module to prevent removing common default branch names

--- Modules/pruneGitBranches.psm1
+++ Modules/pruneGitBranches.psm1
@@ -1,13 +1,17 @@
 function Invoke-PruneGitBranches {
   Write-Host "Pruning Remote..." -ForegroundColor Green
-  git remote prune origin
 
   $AllBranches = git branch -a
   $BranchesToSave = New-Object Collections.Generic.List[String]
   $BranchesToRemove = New-Object Collections.Generic.List[String]
-
+  
   foreach ($Branch in $AllBranches) {
-    if ($Branch.Contains("remotes") -or ($Branch.Contains("develop"))) {
+    $ShouldKeepBranch = [bool](($Branch.Contains("remotes")) `
+                                -or ($Branch.Contains("develop")) `
+                                -or ($Branch.Contains("main")) `
+                                -or ($Branch.Contains("master")) `
+                                -or ($Branch.Contains("trunk")))
+    if ($ShouldKeepBranch) {
       $BranchEnd = $Branch.split("/")[-1]
       $BranchesToSave.Add($BranchEnd)
       Write-Host "Save ${Branch}" -ForegroundColor Blue
@@ -19,13 +23,12 @@ function Invoke-PruneGitBranches {
     if (!$Branch.Contains("remotes") -And !$BranchesToSave.Contains($BranchEnd)) {
       $BranchesToRemove.Add($Branch)
       Write-Host "Remove ${Branch}" -ForegroundColor Red
-            
     }
   }
 
   $Continue = Read-Host -Prompt "Continue? (y/n)"
   if ('y' -ne $Continue) {
-    Write-Host "No commands sent. Exiting script" -ForegroundColor Red
+    Write-Host "No branches deleted. Exiting script" -ForegroundColor Red
     break;
   }
 
