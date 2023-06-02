e398cf872f4c218a9f533f812487ffdc7c834ef7
Cole Sulak <97049435+c0l3yni@users.noreply.github.com>
2023-06-02 13:13:49 -0500

    changed @TEK to @comutername in pShellPrompt

--- Modules/pShellPrompt.psm1
+++ Modules/pShellPrompt.psm1
@@ -1,7 +1,6 @@
 function loadPrompt {
   Write-Host "$ENV:USERNAME" -NoNewline -ForegroundColor DarkYellow
-  # Write-Host "@$ENV:COMPUTERNAME" -NoNewline -ForegroundColor DarkMagenta <- use this instead of below if you want to display your machine name instead of "TEK"
-  Write-Host "@TEK" -NoNewline -ForegroundColor DarkMagenta
+  Write-Host "@$ENV:COMPUTERNAME" -NoNewline -ForegroundColor DarkMagenta
   Write-Host " " -NoNewline -ForegroundColor DarkGray
   Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor Magenta
   Write-Host " " -NoNewline -ForegroundColor DarkGray
