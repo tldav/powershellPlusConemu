a49e26619cce77a7540ec33adbc006d9716d9dab
tldav <tldavis09@yahoo.com>
2023-07-01 13:26:16 -0500

    fix issue w/ pruneGit module and add initial user prompt for exit option before purning remote

--- Modules/pruneGitBranches.psm1
+++ Modules/pruneGitBranches.psm1
@@ -1,41 +1,48 @@
 # Contributed by: Donavyn Haley -> https://github.com/DonavynHaley
-
 function Invoke-PruneGitBranches {
-  Write-Host "Pruning Remote..." -ForegroundColor Green
+  $Start = Read-Host -Prompt "Run 'git remote prune origin'? (y/n)"
 
-  $AllBranches = git branch -a
-  $BranchesToSave = New-Object Collections.Generic.List[String]
-  $BranchesToRemove = New-Object Collections.Generic.List[String]
+  if ($Start -eq "y") {
+    Write-Host "Pruning Remote..." -ForegroundColor Green
+    git remote prune origin
+
+    $AllBranches = git branch -a
+    $BranchesToSave = New-Object Collections.Generic.List[String]
+    $BranchesToRemove = New-Object Collections.Generic.List[String]
   
-  foreach ($Branch in $AllBranches) {
-    $ShouldKeepBranch = [bool](($Branch.Contains("remotes")) `
-                                -or ($Branch.Contains("develop")) `
-                                -or ($Branch.Contains("main")) `
-                                -or ($Branch.Contains("master")) `
-                                -or ($Branch.Contains("trunk")))
-    if ($ShouldKeepBranch) {
+    foreach ($Branch in $AllBranches) {
+      $ShouldKeepBranch = [bool](($Branch.Contains("remotes")) `
+          -or ($Branch.Contains("develop")) `
+          -or ($Branch.Contains("main")) `
+          -or ($Branch.Contains("master")) `
+          -or ($Branch.Contains("trunk")))
+      if ($ShouldKeepBranch) {
+        $BranchEnd = $Branch.split("/")[-1]
+        $BranchesToSave.Add($BranchEnd)
+        Write-Host "Save ${Branch}" -ForegroundColor Blue
+      }
+    }
+
+    foreach ($Branch in $AllBranches) {
       $BranchEnd = $Branch.split("/")[-1]
-      $BranchesToSave.Add($BranchEnd)
-      Write-Host "Save ${Branch}" -ForegroundColor Blue
+      if (!$Branch.Contains("remotes") -And !$BranchesToSave.Contains($BranchEnd)) {
+        $BranchesToRemove.Add($Branch)
+        Write-Host "Remove ${Branch}" -ForegroundColor Red
+      }
+    }
+
+    $Continue = Read-Host -Prompt "Continue? (y/n)"
+    if ('y' -ne $Continue) {
+      Write-Host "No branches deleted. Exiting script" -ForegroundColor Red
+      break;
+    }
+
+    foreach ($Branch in $BranchesToRemove) {
+      git branch -D $Branch.Trim()
     }
   }
-
-  foreach ($Branch in $AllBranches) {
-    $BranchEnd = $Branch.split("/")[-1]
-    if (!$Branch.Contains("remotes") -And !$BranchesToSave.Contains($BranchEnd)) {
-      $BranchesToRemove.Add($Branch)
-      Write-Host "Remove ${Branch}" -ForegroundColor Red
-    }
-  }
-
-  $Continue = Read-Host -Prompt "Continue? (y/n)"
-  if ('y' -ne $Continue) {
+  else {
     Write-Host "No branches deleted. Exiting script" -ForegroundColor Red
-    break;
-  }
-
-  foreach ($Branch in $BranchesToRemove) {
-    git branch -D $Branch.Trim()
   }
 }
 
